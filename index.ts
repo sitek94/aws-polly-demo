dbecda727c829a859dd411c8cda20da0a4c2ffe7
Maciek Sitkowski <msitkowski94@gmail.com>
2023-03-09 21:01:58 +0100

    init server and dotenv

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import express from 'express'
+import 'dotenv/config'
 
 const app = express()
 
+console.log(process.env)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(/* html */ `
+    <h1>Hello</h1>
+
+    <script>
+      (async () => {
+        const response = await fetch('/synthesize')
+        console.log(response)
+      })()
+      
+    </script>
+  `)
+})
+
+app.get('/synthesize', async (req, res) => {
+  res.send('Hello')
 })
 
 app.listen(3000, () => {
